5fd14bb411b673ed84d022caf94968754593d941
Mordanis <champbats6@gmail.com>
2014-10-18 20:50:36 -0700

    added files for a solar model

---
+++ solar_model.f95
@@ -0,0 +1,18 @@
+!This program will model the interior of a star
+!It will prompt the user for the mass
+!and composition of the star -- star will be
+!modeled as a zero age main sequence
+
+!This file will only call other files
+!My goal is to make this model as quick
+!as the one included in Carrol & Ostlie
+PROGRAM solar_model
+
+   Implicit None
+   Include 'solar_getvals.f95'
+   Use get_vals
+
+
+   Call get_vals
+
+END PROGRAM solar_model
